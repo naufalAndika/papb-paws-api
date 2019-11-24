5fbdf597494828336569eaade02f3dc5578a5955
Naufal Andika <andikaa.naufal@gmail.com>
2019-11-24 22:01:00 +0700

    Test if user saved in database

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const app = require('../src/app')
 const { setupDatabase } = require('./fixtures/db')
+const User = require('../src/models/user')
 
 const userOne = {
   name: 'Naufal Andika',
@@ -11,9 +12,12 @@ const userOne = {
 
 beforeEach(setupDatabase)
 
-test('Given user data should sign up user', async () => {
-  await request(app)
+test('Given user data when create user should save in database', async () => {
+  const response = await request(app)
     .post('/users')
     .send(userOne)
     .expect(201)
+
+  const user = await User.findById(response.body._id)
+  expect(user).not.toBeNull()
 })
