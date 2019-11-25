6703e3b9c776a82ea539dcebdd1f7728245328cc
Naufal Andika <andikaa.naufal@gmail.com>
2019-11-25 08:24:55 +0700

    Return not found when findById withinvalid id

--- src/routers/shelter.js
+++ src/routers/shelter.js
@@ -29,6 +29,13 @@ router.get('/shelters', async (req, res) => {
 router.get('/shelters/:id', async (req, res) => {
   try {
     const response = await shelterService.findById(req.params.id)
+    console.log(response)
+    const notFound = response === null
+
+    if (notFound) {
+      res.status(404).send()
+    }
+
     res.status(200).send(response)
   } catch (e) {
     res.status(500).send()
