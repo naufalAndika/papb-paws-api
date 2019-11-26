63a3e2146c978e8296fc63d69748d8e04038da65
Naufal Andika <andikaa.naufal@gmail.com>
2019-11-26 10:52:41 +0700

    Refactor upload middleware

--- src/routers/post.js
+++ src/routers/post.js
@@ -1,24 +1,8 @@
 const express = require('express')
 const router = new express.Router()
 const auth = require('../middleware/auth')
-// const upload = require('../middleware/upload')
+const upload = require('../middleware/upload')
 const postService = require('../services/post/index')
-
-const multer = require('multer')
-
-const upload = multer({
-  limits: {
-    fileSize: 1000000
-  },
-  fileFilter(req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return cb(new Error('Please upload an image'))
-    }
-
-    cb(undefined, true)
-  }
-})
-
 
 router.post('/posts', auth, upload.single('photo'), async (req, res) => {
   const data = {
